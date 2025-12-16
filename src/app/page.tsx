e941773a765909f4da79b1833d33d66e93b75060
Jonatahn Bajada <jbajada@ryerson.ca>
2025-12-16 13:03:15 +0100

    updated skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -369,6 +369,18 @@ export default function Home() {
             </div>
             <p>Building beautiful, responsive, and performant frontends.</p>
           </li>
+          <li className="frosted-glass-dark flex items-center space-x-4 rounded-xl p-5">
+            <div className="flex h-16 w-16 items-center justify-center rounded-lg">
+              <svg
+                className="h-12 w-12 fill-[#9c97b4]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm0 10.99h7c-.53 4.12-3.28 7.79-7 8.94V12H5V6.3l7-3.11v8.8z" />
+              </svg>
+            </div>
+            <p>Implementing security best practices and secure coding standards.</p>
+          </li>
         </ol>
       </section>
       <section
